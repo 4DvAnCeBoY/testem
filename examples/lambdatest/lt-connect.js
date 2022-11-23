33f5283d0d6a83c5db9daa90593499a0d0f9ee2a
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2022-11-23 10:14:48 +0000

    tunnel updates

--- examples/lambdatest/lt-connect.js
+++ examples/lambdatest/lt-connect.js
@@ -2,10 +2,46 @@
 
 var ltTestem = require('lt-testem');
 var pidFile = 'lt_tunnel.pid';
+var lambdaTunnel = require('@lambdatest/node-tunnel');
+const { env } = require('shelljs');
+
+if(process.argv[2]){
+  process.kill(process.argv[2], 'SIGINT');
+  process.exit(0);
+}
+
+var tunnelInstance = new lambdaTunnel();
+// Replace <lambdatest-user> with your user and <lambdatest-accesskey> with your key.
+var tunnelArguments = {
+  user: process.env.LT_USERNAME || '<lambdatest-user>',
+  key: process.env.LT_ACCESS_KEY || '<lambdatest-accesskey>',
+  pidFile : pidFile
+};
+
+process.on('SIGINT', function() {
+  if (tunnelInstance !== null) {
+      tunnelInstance.stop(function(error) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('tunnelInstance Disconnected');
+        process.exit();
+      }
+    });
+  }
+});
+
+tunnelInstance.start(tunnelArguments, function(error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Tunnel Started");
+  }
+});
 
 var opts = {
-  user: process.env.LT_USERNAME,
-  key: process.env.LT_ACCESS_KEY,
+  username: process.env.LT_USERNAME,
+  accessKey: process.env.LT_ACCESS_KEY,
   verbose: true,
   tunnel:true,
   logger: console.log,
